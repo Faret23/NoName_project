d42eea311f050a0d1c81c7186670c285482b700c
Danylo_Halchuk <chinchope23@gmail.com>
2021-04-10 12:41:42 +0300

    Add .gitignore and bot_demo.py

---
+++ Noname_bot_demo.py
@@ -0,0 +1,18 @@
+import telebot
+
+TOKEN = '1497015612:AAHynuIJ5QSg8xR-GTEroo5KJ9EOd2nyzdM'
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
